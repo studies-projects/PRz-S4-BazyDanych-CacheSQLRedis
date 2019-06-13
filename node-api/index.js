68df055923933acd29bc0ecd65e0a18c64f34253
Marcin Pazowski | PC <CaishenNefri@gmail.com>
2019-06-13 16:07:10 +0200

    Add ORM Sequalize

--- node-api/index.js
+++ node-api/index.js
@@ -4,6 +4,27 @@ const app = express()
 const port = 3000
 const db = require('./queries')
 
+var Sequelize = require('sequelize'),
+    sequelize = new Sequelize('chinook', 'postgres', 'docker', {
+        dialect: 'postgres',
+        host: '192.168.0.224',
+        port: 5432,
+
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        },
+    });
+
+sequelize
+    .authenticate()
+    .then(function(err) {
+        console.log('Connection has been established succesfully.');
+    }, function (err) {
+        console.log('Unable to connect to the database:', err);
+    });
+
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
